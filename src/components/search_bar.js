038784bf879856b5a028e4bd6ebedb43a40ec267
Tenzin Tsagong <tsagong@gmail.com>
2017-05-10 11:03:10 -0400

    searching-videos

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,9 +10,14 @@ class SearchBar extends Component {
     return (
       <div className="search-bar">
         <input
-          onChange ={event => this.setState({ term: event.target.value }) } />
+          onChange ={event => this.onInputChange(event.target.value)} />
       </div>
       );
+  }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
   }
 }
 
