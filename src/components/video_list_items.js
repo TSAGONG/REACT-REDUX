ff11808f7d8af7a78865516c0d81ae5ebb50b4d7
Tenzin Tsagong <tsagong@gmail.com>
2017-05-10 10:23:37 -0400

    video-display

--- src/components/video_list_items.js
+++ src/components/video_list_items.js
@@ -1,1 +1,21 @@
-video_list_items.js
+import React from 'react';
+
+const VideoListItem = ({video}) => {
+  const ImageUrl = video.snippet.thumbnails.default.url;
+
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className ="media-object" src={ImageUrl} />
+        </div>
+
+        <div className="media-body">
+          <div className="media-heading">{video.snippet.title}</div>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default VideoListItem;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_details';
 
 const API_KEY = 'AIzaSyDz5xDx8Z8cYFIROA1JWSBYEJEgpAQ8mrY';
 
+
 // create a new component. this component should
 // produce some HTML
-//React.createElement
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 
